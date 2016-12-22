f082feeefda8be0d87af07027846c2702e5110fe
micahcourey <micahjcourey@gmail.com>
2016-12-21 21:32:24 -0800

    make use of the jwt-simple module to create a token to identify a user when they signup for the app

--- server/models/user.js
+++ server/models/user.js
@@ -9,15 +9,20 @@ const userSchema = new Schema({
 });
 
 // On Save Hook, encrypt password
+// Before saving a model, run this function
 userSchema.pre('save', function(next) {
+  // Get access to the user model 
   const user = this;
 
+  // Generate a salt then run callback
   bcrypt.genSalt(10, function(err, salt) {
     if (err) { return next(err); }
 
+    // Hash (encrypt) password using salt
     bcrypt.hash(user.password, salt, null, function(err, hash) {
       if (err) { return next(err); }
 
+      // Overwrite plain text password with encrypted password
       user.password = hash;
       next(); 
     });
