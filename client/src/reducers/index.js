c3c0c5fb22f7ed1e777d3d55085ffd81d89c0416
micahcourey <micahjcourey@gmail.com>
2017-02-28 23:34:28 -0800

    scaffold Signin form component

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import { reduce as form } from 'redux-form';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  form
 });
 
 export default rootReducer;
